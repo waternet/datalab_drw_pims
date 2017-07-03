a2b0a9c7543d6620589f70e229a581f5622d8657
Breinbaas | Rob van Putten <breinbaas@gmail.com>
2017-07-03 14:25:15 +0200

    Added a few new wrappers and started the time val wrapper

--- test_wnpims.py
+++ test_wnpims.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, datetime
 import wnpims
 
 api = wnpims.API()
@@ -9,6 +9,11 @@ class TestWNPimsCurrVal(unittest.TestCase):
         # todo: test if server is online
         self.assertEqual(api.get_curr_val("PLV-SB-001_850").shape, (1,9))
         self.assertEqual(api.get_curr_val("PLV-SB-001_XXX"), None)
+
+    def test_get_trend(self):
+        start = datetime.datetime(2015,1,1)
+        end = datetime.datetime(2016,1,1)
+        self.assertEqual(api.get_trend(tagname='VVZ1GA01GM001_VD', alist='GAS-DW-VD', start=start, end=end,calc='actual').shape, (6,9))
 
 if __name__=="__main__":
     unittest.main()
--- wnpims.py
+++ wnpims.py
@@ -19,7 +19,7 @@ class API:
         """This function sends a request and return the text of the response. It deals in a uniform way with possible errors.
 
         INPUT
-        url:    url of the website to retrieve data from
+        url   : url of the website to retrieve data from
         params: a dictionary of parameters
         output: output format of the result (default pandas)
 
@@ -33,6 +33,7 @@ class API:
         self._log("starting request to %s" % finalurl)
 
         resp = requests.get(finalurl)
+        print resp.text
         if resp.status_code != 200:
             self._log("Got statuscode %s" % resp.status_code)
             return None
@@ -87,6 +88,66 @@ class API:
         params = {'tagnaam':tagname}
         return self._get_request_result(url, params)
 
+    def get_trend(self, tagname, alist, start, end, interval=1, interval_unit='dagen', calc='actual'):
+        """Wrapper on the get trend url
+
+        INPUT
+        tagname      : tagname according to the available taglist (wildcards available)
+        alist        : TODO, wat betekent dit?
+        start        : start date (datetime)
+        end          : end date (datetime)
+        interval     : interval of measurements (default=1)
+        interval_unit: unit of the interval (dagen (default), uren, minuten, seconden)
+        calc         : type of calculation (actual (default), avg, min, max, int_lin, int_step)
+
+        OUTPUT
+        Pandas dataframe or None in case of error
+        """
+        url = ww.URL_GET_TREND
+        sstart = start.strftime("%d-%m-%Y")
+        send = end.strftime("%d-%m-%Y")
+        params = {'tagnaam':tagname, 'lijst':alist, 'starttijd':sstart, 'eindtijd':send,
+        'periode':interval, 'periode eenheid':interval_unit, 'calculatie':calc}
+        return self._get_request_result(url, params)
+
+    def get_trend_pivot(self, tagname, alist, start, end, interval=1, interval_unit='dagen', calc='actual', time_axis=1):
+        """Wrapper on the get trend pivot url
+
+        INPUT
+        tagname      : tagname according to the available taglist (wildcards available)
+        alist        : TODO, wat betekent dit?
+        start        : start date (datetime)
+        end          : end date (datetime)
+        interval     : interval of measurements (default=1)
+        interval_unit: unit of the interval (dagen (default), uren, minuten, seconden)
+        calc         : type of calculation (actual (default), avg, min, max, int_lin, int_step)
+        time_axis    : get time axis (default=1)
+
+        OUTPUT
+        Pandas dataframe or None in case of error
+        """
+        url = ww.URL_GET_TREND_PIVOT
+        sstart = start.strftime("%d-%m-%Y")
+        send = end.strftime("%d-%m-%Y")
+        params = {'tagnaam':tagname, 'lijst':alist, 'starttijd':sstart, 'eindtijd':send,
+        'periode':interval, 'periode eenheid':interval_unit, 'calculatie':calc, 'tijdsas':time_axis}
+        return self._get_request_result(url, params)
+
+    def get_time_val(self, tagname, timestamp):
+        """Wrapper on the get time val url
+
+        INPUT
+        tagname      : tagname according to the available taglist (wildcards available)
+        timestamp    : datetime of the value
+
+        OUTPUT
+        Pandas dataframe or None in case of error
+        """
+        url = ww.URL_GET_TIME_VAL
+        stimestamp = timestamp.strftime("%d/%m/%Y %H:%M")
+        params = {'tagnaam':tagname, 'timestamp':stimestamp}
+        return self._get_request_result(url, params)
+
     def print_log(self):
         """Prints the logbook as seperated lines"""
         for line in self._logbook:
@@ -95,9 +156,23 @@ class API:
 # EXAMPLES
 if __name__=="__main__":
     api = API()
-    df = api.get_curr_val("VVZ1GA01GM00__VD")
-    if df is None:
-        print "Fout bij de aanroep, hieronder zie je de log"
-        api.print_log()
-    else:
-        print df        
+
+    #get current value
+    #df1 = api.get_curr_val("VVZ1GA01GM00__VD")
+
+    #get trend
+    #start = datetime.datetime(2015,1,1)
+    #end = datetime.datetime(2015,12,31)
+    #df2 = api.get_trend(tagname='VVZ1GA01GM001_VD', alist='GAS-DW-VD', start=start, end=end)
+
+    #get trend pivot
+    #start = datetime.datetime(2015,1,1)
+    #end = datetime.datetime(2015,12,31)
+    #df3 = api.get_trend_pivot(tagname='VVZ1GA01GM001_VD', alist='GAS-DW-VD', start=start, end=end)
+    #print df3
+
+    testdate = datetime.datetime(2016,5,12,19,0)
+    print testdate
+    df4 = api.get_time_val("2W325WK01FIT001", testdate) #werkt nog niet goed! 
+
+    api.print_log()
